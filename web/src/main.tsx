46e7fefb2d32d121e5e95107d335476cd9e26b62
PendoWolf <pendo+githubnovus@qawolf.email>
2026-07-29 14:46:13 -0400

    Added visitor id's for segments

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+Let visitorId = localStorage.getItem(‘demo-visitor-id’);
+If (!visitorId) {
+	visitorId = ‘visitor-‘ + Math.random().toString(36).slice(2);
+	localStorage.setItem(‘demo-visitor-id’, visitorId); 
+}
 pendo.initialize({
   visitor: {
-    id: ''
+  	id: visitorId
   }
 });
 
