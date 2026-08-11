3500e28dbe043d3f9dc3d4584993c4b2c7d27547
PendoWolf <pendo+githubnovus@qawolf.email>
2026-08-11 09:37:47 -0400

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,13 +3,23 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 let visitorId = localStorage.getItem('demo-visitor-id');
+let accountId = localStorage.getItem('demo-account-id');
+
 if (!visitorId) {
   visitorId = 'visitor-' + Math.random().toString(36).slice(2);
   localStorage.setItem('demo-visitor-id', visitorId);
 }
+if (!accountId) {
+  accountId = 'account-' + Math.random().toString(36).slice(2);
+  localStorage.setItem('demo-account-id', accountId);
+}
+
 pendo.initialize({
   visitor: {
     id: visitorId,
+  },
+  account: {
+    id: accountId,
   },
 });
 
